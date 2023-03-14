ad47773d6854f207af29be45ff18755cfed7eb6e
Isanderr <isander@wp.pl>
2023-03-14 13:39:47 +0100

    add new hero image

---
+++ .history/src/App_20230313164946.jsx
@@ -0,0 +1,11 @@
+
+const App = () =>{
+
+  return (
+    <div>
+      3D Security Engineer Portfolio
+    </div>
+  )
+}
+
+export default App
---
+++ .history/src/components/canvas/Computers_20230314124316.jsx
@@ -0,0 +1,43 @@
+import React, { Suspense, useEffect, useState } from "react";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
+
+import CanvasLoader from "../Loader";
+
+const Computers = ({ isMobile }) => {
+  
+
+  return (
+    <div class="flex h-screen w-full bg-blue-400 justify-center items-center" style={{height:"50px", width:"50px"}}>
+      <img src="../assets/cybersecHero.png" style={{height:"50px",  width:"50px"}}></img>
+   </div>
+  );
+};
+
+const ComputersCanvas = () => {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    // Add a listener for changes to the screen size
+    const mediaQuery = window.matchMedia("(max-width: 500px)");
+
+    // Set the initial value of the `isMobile` state variable
+    setIsMobile(mediaQuery.matches);
+
+    // Define a callback function to handle changes to the media query
+    const handleMediaQueryChange = (event) => {
+      setIsMobile(event.matches);
+    };
+
+    // Add the callback function as a listener for changes to the media query
+    mediaQuery.addEventListener("change", handleMediaQueryChange);
+
+    // Remove the listener when the component is unmounted
+    return () => {
+      mediaQuery.removeEventListener("change", handleMediaQueryChange);
+    };
+  }, []);
+
+};
+
+export default ComputersCanvas;
